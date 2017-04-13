96df70f9b1a00328cd6301ee99b34a6c21088f35
Julio Cruz <atlascruz009@gmail.com>
2017-04-12 17:11:36 -0700

    Fixed listview issue

--- app/src/main/java/com/fitme/fitme/FindBuddyActivity.java
+++ app/src/main/java/com/fitme/fitme/FindBuddyActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -25,14 +24,10 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -40,36 +35,32 @@ public class FindBuddyActivity extends AppCompatActivity
         implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
-    List<Address> addresses;
+    private Button searchButton;
+    private Button removeButton;
+    private TextView displayLocalUsers;
+    private ListView localUsersListView;
+    private TextView activeUserTextView;
+
+    private String userID;
+    private String username;
+    private UserLocation userLocation;
+    int count = 0;
+
+    // Firebase
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser user;
+
+    // Firebase Adapter
+    private FirebaseListAdapter<UserLocation> mAdapter;
+
+    LocationCalculator locationCalculator;
+
+    // Location
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     Geocoder geocoder;
-
-    int count = 0;
-
-    private FirebaseUser user;
-    private FirebaseDatabase mFirebaseDatabase;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseListAdapter<UserLocation> firebaseListAdapter;
-    private DatabaseReference myRef;
-
-    private List<UserLocation> locals;
-    private List<UserLocation> usersNearYou;
-    private Button searchButton;
-    private Button removeButton;
-    private String userID;
-    private TextView displayLocalUsers;
-    //private TextView email;
-    //private TextView city;
-    private ListView localUsersListView;
-    private LocationCalculator locationCalculator;
-
-
-    private TextView textViews;
-
-
-    UserLocation userLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,24 +72,27 @@ public class FindBuddyActivity extends AppCompatActivity
         removeButton = (Button) findViewById(R.id.removeButton);
         displayLocalUsers = (TextView) findViewById(R.id.activeUserTextView);
         localUsersListView = (ListView) findViewById(R.id.localUsers);
-
-        textViews= (TextView) findViewById(R.id.textViews);
+        activeUserTextView = (TextView) findViewById(R.id.activeUserTextView);
 
         //Remove button to Invisible
         removeButton.setVisibility(View.INVISIBLE);
 
-        // Init userLocation Onject and List
-        userLocation = new UserLocation();
-        locals = new ArrayList<UserLocation>();
-
-        // Get Firebase Instances and Refrences
-        mAuth = FirebaseAuth.getInstance();
+        // Firebase
         mFirebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = mFirebaseDatabase.getReference().child("locations");
-        // Gets logged in users unique ID
-        user = mAuth.getCurrentUser();
-        userID = user.getUid();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("location");
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
+        // Gets logged in users unique ID
+        user = mFirebaseAuth.getCurrentUser();
+        userID = user.getUid();
+        username = user.getDisplayName();
+        activeUserTextView.setText(username);
+
+        locationCalculator = new LocationCalculator();
+        userLocation = new UserLocation();
+
+
+        // Location
         // Geocoder converts longitude and latitude to street address
         geocoder = new Geocoder(this, Locale.getDefault());
 
@@ -114,141 +108,62 @@ public class FindBuddyActivity extends AppCompatActivity
         localUsersListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String key = firebaseListAdapter.getRef(position).getKey();
-                Toast.makeText(getApplicationContext(), key, Toast.LENGTH_LONG).show();
+                String key = mAdapter.getRef(position).getKey();
                 Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
                 intent.putExtra("LOCATIONS_ID", key);
                 startActivity(intent);
             }
         });
-
-        final List<UserLocation> list = new ArrayList<>();
-
-        firebaseListAdapter = new FirebaseListAdapter<UserLocation>(this,
-                UserLocation.class, android.R.layout.simple_list_item_1, myRef) {
-
-                    @Override
-                    protected void populateView(View v, UserLocation model, int position) {
-
-                        v = View.inflate(getApplicationContext(), R.layout.location_item_list,
-                                null);
-                        TextView email = (TextView) v.findViewById(R.id.email);
-                        TextView city = (TextView) v.findViewById(R.id.city);
-
-                        email.setText(model.getEmail());
-                        Log.d("DISPLAY EMAIL", model.getEmail());
-                        city.setText(model.getCity());
-                        Log.d("DISPLAY CITY", model.getCity());
-
-                    }
-                };
-        localUsersListView.setAdapter(firebaseListAdapter);
-
     }
 
     /**
      * Click Event when search for partner button is clicked
+     *
      * @param view
      */
     public void searchButtonClicked(View view) {
-
         // Get User Location and add it into the database
-        getMyLocation();
+        retrieveUserLocation();
+
         searchButton.setVisibility(View.INVISIBLE);
         removeButton.setVisibility(View.VISIBLE);
-        // Grab list of users in locations table
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                showData(dataSnapshot);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 
     /**
      * Click Event when remove location button is clicked
+     *
      * @param view
      */
     public void removeLocationClicked(View view) {
-        myRef.child(userID).removeValue();
+        mDatabaseReference.child(userID).removeValue();
         searchButton.setVisibility(View.VISIBLE);
         removeButton.setVisibility(View.INVISIBLE);
     }
 
-    /**
-     * Will read from the locations table in database
-     * @param dataSnapshot
-     */
-    private void showData(DataSnapshot dataSnapshot) {
-
-        // loops through all children in locations table
-        for (DataSnapshot ds : dataSnapshot.getChildren()) {
-            UserLocation uLocation = new UserLocation();
-            uLocation.setEmail(ds.getValue(UserLocation.class).getEmail());
-            uLocation.setLatitude(ds.getValue(UserLocation.class).getLatitude());
-            uLocation.setLongitude(ds.getValue(UserLocation.class).getLongitude());
-            uLocation.setCity(ds.getValue(UserLocation.class).getCity());
-
-            // Add user to list
-            locals.add(uLocation);
-        }
-
-        Log.d("COUNT", Integer.toString(locals.size()));
-        // Call comparee Locations to get users near you
-        compareLocations(locals);
-        // Display local users to listview
-        displayLocalUsers(usersNearYou);
-    }
-
-    /**
-     * Will compare users distances
-     * @param locals
-     */
-    private void compareLocations(List<UserLocation> locals) {
-        locationCalculator = new LocationCalculator();
-        usersNearYou = new ArrayList<UserLocation>();
-
-        // Check users to see if in 5 mile radius (8.04672 km = 5 mi)
-        for(int i = 0; i < locals.size(); i++) {
-            if(locationCalculator.calculateDistance(userLocation,locals.get(i)) < 8.04672) {
-                usersNearYou.add(locals.get(i)); // Add user to list
-            }
-        }
-    }
-
-    /**
-     * Retrieve device location
-     **/
-    private void getMyLocation() {
+    private void retrieveUserLocation() {
         try {
             // Returns last know location from user
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
             // Checks if location can be found
             if (mLastLocation != null) {
-                addresses = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+                List<Address> addresses = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
                 String city = addresses.get(0).getLocality();
 
                 // Set userLocation object variables
+
                 userLocation.setId(count++);
                 userLocation.setEmail(user.getEmail());
                 userLocation.setLatitude(mLastLocation.getLatitude());
                 userLocation.setLongitude(mLastLocation.getLongitude());
                 userLocation.setCity(city);
 
-                // Display Toast(pop-up message displaying email, latitude, longitude and city
-                Toast.makeText(getApplicationContext(), "EMAIL: " + userLocation.getEmail() + " LATITUDE: " + userLocation.getLatitude() + " LONGITUDE: " + userLocation.getLongitude() + " CITY: " +
-                        userLocation.getCity(), Toast.LENGTH_LONG).show();
-
                 // Inset into database
-                insertIntoDatabase(userLocation);
+                mDatabaseReference.child(userID).setValue(userLocation);
+
+                // Display users near each other
+                findLocalUsers();
             }
             else {
                 // Location could not be found
@@ -260,24 +175,25 @@ public class FindBuddyActivity extends AppCompatActivity
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    /**
-     * Adds users to database
-     * @param userLocation
-     */
-    private void insertIntoDatabase(UserLocation userLocation) {
-        myRef.child(userID).setValue(userLocation);
+    private void findLocalUsers() {
+        mAdapter = new FirebaseListAdapter<UserLocation>(this, UserLocation.class, android.R.layout
+                .two_line_list_item, mDatabaseReference) {
+            @Override
+            protected void populateView(View view, UserLocation user, int position) {
+
+                if(locationCalculator.calculateDistance(userLocation, user) <  8.04672)  // 5 miles
+                {
+                    ((TextView)view.findViewById(android.R.id.text1)).setText(user.getEmail());
+                    ((TextView)view.findViewById(android.R.id.text2)).setText(user.getCity());
+                }
+
+            }
+        };
+        localUsersListView.setAdapter(mAdapter);
     }
 
-    /**
-     * Display list of usersNearYou onto ListView
-     * @param usersNearYou
-     */
-    private void displayLocalUsers(final List<UserLocation> usersNearYou) {
-        Log.d("DISPLAY COUNT", Integer.toString(usersNearYou.size()));
-    }
     @Override
     protected void onStart() {
         mGoogleApiClient.connect();
